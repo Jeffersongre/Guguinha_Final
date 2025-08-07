05adc9dbfd48154580a4296d66e8b721dfc4a77b
Jefferson Gregorio <jeffoliveira308@gmail.com>
2025-08-06 21:46:49 -0300

    melhoria

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Guguinha_Final/', // <- nome exato do repositório
+  base: '/', // <- nome exato do repositório
   plugins: [react()],
 })
